bb5451f7081c22704b2e42c7fb76978c35de66de
Stephen James <stephenorjames@gmail.com>
2018-10-16 00:57:01 -0400

    Create loans.Client model
    This will allow us to store information about individuals reserving
    and checking out items.
    
    In the future, this can be linked with users.User to provide additional
    features for users who are signed up (e.g. listing items that they
    currently have checked out).

--- dam/loans/models.py
+++ dam/loans/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """A client is someone who will be reserving or checking out items."""
+    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
