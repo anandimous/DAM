b48265ccacf9dea00e9b762e701e08450ada6e0b
Stephen James <stephenorjames@gmail.com>
2018-10-16 01:03:27 -0400

    Create ItemReservation model
    This will be used to track when items are reserved by clients.

--- dam/loans/models.py
+++ dam/loans/models.py
@@ -6,3 +6,10 @@ class Client(models.Model):
     email = models.EmailField(max_length=255)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+
+class ItemReservation(models.Model):
+    item = models.ForeignKey('inventory.Item', models.CASCADE)
+    client = models.ForeignKey(Client, models.CASCADE)
+    reserved_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
