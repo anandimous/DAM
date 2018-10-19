6532e17f97d551d89b8325643ce25496aac09cc5
Lawzeem <lawzeeml@buffalo.edu>
2018-10-19 19:29:28 -0400

    Empty

--- dam/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import showDash
-from ..inventory.views import *
-urlpatterns = [
-    url(r'^$', showDash, name='showDash'),
-]
--- dam/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.views import LoginView as BaseLogInView
-
-from .forms import AuthenticationForm
-
-
-class LogInView(BaseLogInView):
-    template_name = 'users/log_in.html'
-    form_class = AuthenticationForm
