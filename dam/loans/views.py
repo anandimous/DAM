b215446e0e44d88ad0d370131576f953ea3f446c
Lawreen <lawreenl@buffalo.edu>
2018-10-19 13:41:59 -0400

    added templates in Loans including base.html and loanItem.html which has skeletons for loaning item

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from ..inventory import models
 # Create your views here.
+
+def index(request):
+    items = models.Item.objects.all()
+    args={'items':items}
+    return render(request, 'loans/loanItem.html', args)
--- dam/urls.py
+++ dam/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('dam.users.urls')),
+    path('loans/', include('dam.loans.urls')),
 ]
