006990e0037c6ec8902e9835f1dbaf742509ea10
Lawzeem <lawzeeml@buffalo.edu>
2018-10-19 19:18:11 -0400

    Updated Dashboard. Ready for pull.

--- dam/index/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import showMain
-from ..inventory.views import *
-urlpatterns = [
-    url(r'^$', showMain, name='showMain'),
-    url(r'^dam/inventory/$', index, name='index')
-]
--- dam/settings.py
+++ dam/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'dam.inventory',
     'dam.loans',
     'dam.users',
-    'dam.index',
     'dam.dashboard',
 ]
 
