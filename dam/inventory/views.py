41bad28c24f3ad184b73767c4ea3bb34107b1b62
Lawzeem <lawzeeml@buffalo.edu>
2018-10-23 12:24:26 -0400

    Used ItemManager in inventory views to find out if item is available or not. Results now show if an item is available or unavailable

--- dam/inventory/views.py
+++ dam/inventory/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from ..inventory import models
-# Create your views here.
 
 def index(request):
-    items = models.Item.objects.all()
+    items = models.Item.objects.with_availability()
     args={'items':items}
     return render(request, 'inventory/inventory.html', args)
-
-
