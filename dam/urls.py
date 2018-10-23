80fc664821ebde7443c18afd30861a6ee220ec60
Lawzeem <lawzeeml@buffalo.edu>
2018-10-23 16:36:08 -0400

    Branched from Develop, all Items shown including if available

--- dam/urls.py
+++ dam/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('users/', include('dam.users.urls')),
     path('loans/', include('dam.loans.urls')),
     path('dashboard/', include('dam.dashboard.urls')),
+    path('results/', include('dam.inventory.urls')),
 ]
