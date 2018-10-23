1f3c5fc454521ddae11b2a4ef77a69fafa71ba38
Lawreen <lawreenl@buffalo.edu>
2018-10-23 17:53:35 -0400

    figuring out the post/get requests. Also created a form

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import Http404
+from ..inventory import models
+from dam.loans.models import ItemReservation, ItemLoan
+from dam.loans.forms import LoansForm
 
 # Create your views here.
+def reservations(request, reservation_id):
+    try:
+        reserved = ItemReservation.objects.get(id=reservation_id)
+    except ItemReservation.DoesNotExist:
+        raise Http404('Reservation Does not Exist!')
+
+
+    args = {'reserved': reserved,
+            'user':request.user
+            }
+    return render(request, 'loans/loanItem.html', args)
+
+def get(self, request):
+    form = LoansForm()
+    return render(request,'loans/loanItem.html', {'form': form})
+
+def post(self, request):
+    form = LoansForm(request.post)
+    args = {'form': form,
+                }
+    return render(request, 'loans/loanItem.html', args)
