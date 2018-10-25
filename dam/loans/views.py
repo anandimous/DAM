c8047caef3ab568b2ed58ed0b3e1aeb780922f98
Harshita Girase <harshitagirase97@gmail.com>
2018-10-24 23:28:05 -0400

    Added view

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages 
-
+from django import forms
 # Create your views here.
+
+class UserInputForm(forms.Form):
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+
 def checkIfItemAvailable(request): 
     if request.method == 'POST':
         if(Item.objects.with_availability().get(id=item_id).available() > 0:
