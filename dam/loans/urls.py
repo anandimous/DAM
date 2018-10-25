c86fe1a096ad216e87d4a466d6b344d27b69facd
Harshita Girase <harshitagirase97@gmail.com>
2018-10-25 02:25:08 -0400

    Changes made, resolved conflicts in urls.py and views.py

--- dam/loans/urls.py
+++ dam/loans/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('<int:reservation_id>/', reservations, name='reservations'),
     path('loans', allres, name='allres'),
     path('returns', allrets, name='allrets'),
-]   path('',checkIfItemAvailable, name='issue'),
     path('/reserve/<int:item_id>/',checkIfItemAvailable, name='reserve')
-
 ]
