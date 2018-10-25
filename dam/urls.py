f04e37b3f9ab565c9f730a26abda5318e6c90ec9
Lawreen <lawreenl@buffalo.edu>
2018-10-24 21:02:01 -0400

    changed url for homepage

--- dam/urls.py
+++ dam/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dam.dashboard.urls')),
+    path('', include('dam.inventory.urls')),
     path('users/', include('dam.users.urls')),
-    path('results/', include('dam.inventory.urls')),
     path('loans/', include('dam.loans.urls'))
 ]
