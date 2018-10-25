1f7c28537a163e1c606a5ee5ce512393085743ec
Harshita Girase <harshitagirase97@gmail.com>
2018-10-25 00:06:21 -0400

    Views modified, included inputs

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -41,13 +41,13 @@ def allrets(request):
     rets = ItemLoan.objects.filter(returned_at__isnull=True)
     args = {'returns': rets}
     return render(request, 'loans/allReturns.html', args)
-
-def checkIfItemAvailable(request): 
+    
+def checkIfItemAvailable(request, item_id, first_name, last_name, email): 
     if request.method == 'POST':
         if(Item.objects.with_availability().get(id=item_id).available() > 0:
             messages.success(request, 'Your item has been reserved! You can pick it up from Baldy 19')
         else:
             messages.error(request, 'Your item was not reserved. Please go back and reserve the item again.')
-            
-    return redirect('/results/details')
+
+    return redirect('/details',input_id=item_id)
 
