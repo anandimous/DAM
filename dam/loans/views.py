5960db3f88e77b380390bb20a77eda30093bc345
Harshita Girase <harshitagirase97@gmail.com>
2018-10-24 20:43:22 -0400

    Deleted unnecessary code

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -4,8 +4,6 @@ from django.contrib import messages
 # Create your views here.
 def render(request): 
     if request.method == 'POST':
-        #if item exists
-        #is available in database 
         if(Item.objects.with_availability().get(id=item_id).available() > 0 and Item.exists()):
             messages.set_level(request, messages.SUCCESS)
             messages.success(request, 'Your item has been reserved! You can pick it up from Baldy 19')
