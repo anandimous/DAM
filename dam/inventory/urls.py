8c1381199be8abfcc43bea2941bbd635028dc9bb
Aniruddha Nandi <anandi@buffalo.edu>
2018-10-24 22:55:55 -0400

    item details page initialized and connected to item model

--- dam/inventory/urls.py
+++ dam/inventory/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index
-
+from .views import *
 
 app_name = 'inventory'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('details/<int:input_id>', item_details, name='item-details')
 ]
--- dam/inventory/views.py
+++ dam/inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from ..inventory import models
-# Create your views here.
+
 
 def index(request):
     items = models.Item.objects.with_availability()
@@ -8,3 +8,7 @@ def index(request):
     return render(request, 'inventory/inventory.html', args)
 
 
+def item_details(request, input_id):
+    item_id = input_id
+    item = models.Item.objects.get(pk=item_id)
+    return render(request, 'inventory/details.html', {'item_name': item.name, 'item_description': item.description})
