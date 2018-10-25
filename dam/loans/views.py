6fc672b9329f8af05c0a4ff1721a993ce223f295
Harshita Girase <harshitagirase97@gmail.com>
2018-10-24 20:39:41 -0400

    Views file changed

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages 
 
 # Create your views here.
+def render(request): 
+    if request.method == 'POST':
+        #if item exists
+        #is available in database 
+        if(Item.objects.with_availability().get(id=item_id).available() > 0 and Item.exists()):
+            messages.set_level(request, messages.SUCCESS)
+            messages.success(request, 'Your item has been reserved! You can pick it up from Baldy 19')
+        else:
+            messages.error(request, 'Your item was not reserved. Please go back and reserve the item again.')
+
