4705f160bf3bd42ef7231a18a7f9351c2433a6fd
Aniruddha Nandi <anandi@buffalo.edu>
2018-10-25 01:57:35 -0400

    switched div to form for validation routing

--- dam/inventory/urls.py
+++ dam/inventory/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import index, item_details
 
 app_name = 'inventory'
 
--- dam/inventory/views.py
+++ dam/inventory/views.py
@@ -4,11 +4,13 @@ from ..inventory import models
 
 def index(request):
     items = models.Item.objects.with_availability()
-    args={'items':items}
+    args={'items': items}
     return render(request, 'inventory/inventory.html', args)
 
 
 def item_details(request, input_id):
+    if request.method == 'POST':
+        pass
     item_id = input_id
     item = models.Item.objects.get(pk=item_id)
-    return render(request, 'inventory/details.html', {'item_id': item_id, 'item_name': item.name, 'item_description': item.description})
+    return render(request, 'inventory/details.html', {'item_id': item.id, 'item_name': item.name, 'item_description': item.description})
