7adbc0aaa674a4fd338c25fb9aff279683dfec88
Harshita Girase <harshitagirase97@gmail.com>
2018-10-25 05:00:44 -0400

    Removed unnecessary tests

--- dam/loans/tests.py
+++ dam/loans/tests.py
@@ -28,20 +28,3 @@ class ValidateTestCases(TestCase):
         response= self.client.post('/loans/reserve/3')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['id'],'-2')
-
-
-   # @mock.patch ('app.utils.requests.get')
-    #def valid_email(self, mock_get):
-        #mock_response= mock.Mock()
-        #expected_val= {
-           # 'first_name':'Matthew',
-           # 'last_name':'Hertz',
-            #'email':'mhertz@buffalo.edu'
-            #'item_id':'2'
-        #}
-        #mock_response.json.return_value= expected_val
-        #mock_get.return_value= mock_response
-        #result= ValidateTestCases()
-
-    #def invalid_email(self, mock_get):
-         
