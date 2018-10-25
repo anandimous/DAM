0aebaad3682cc0823fc9723707166f24858d565b
Harshita Girase <harshitagirase97@gmail.com>
2018-10-25 00:06:21 -0400

    Views modified, included inputs

--- dam/loans/views.py
+++ dam/loans/views.py
@@ -3,18 +3,12 @@ from django.contrib import messages
 from django import forms
 # Create your views here.
 
-class UserInputForm(forms.Form):
-    email = forms.EmailField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-
-
-def checkIfItemAvailable(request): 
+def checkIfItemAvailable(request, item_id, first_name, last_name, email): 
     if request.method == 'POST':
         if(Item.objects.with_availability().get(id=item_id).available() > 0:
             messages.success(request, 'Your item has been reserved! You can pick it up from Baldy 19')
         else:
             messages.error(request, 'Your item was not reserved. Please go back and reserve the item again.')
-            
-    return redirect('/results/details')
+
+    return redirect('/details',input_id=item_id)
 
