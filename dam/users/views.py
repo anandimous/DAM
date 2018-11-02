c26371ab16d93910e9d8a8adf27ac434bacff6c7
Stephen James <stephenorjames@gmail.com>
2018-11-02 12:30:58 -0400

    Error message when logging in with invalid email
    Fixes #117

--- dam/users/views.py
+++ dam/users/views.py
@@ -7,6 +7,16 @@ class LogInView(BaseLogInView):
     template_name = 'users/log_in.html'
     form_class = AuthenticationForm
 
+    def get_context_data(self, **kwargs):
+        form = kwargs.get('form')
+        form_errors = []
+        if form is not None:
+            for error_list in form.errors.as_data().values():
+                for error in error_list:
+                    form_errors.extend(error.messages)
+        form_errors_str = '. '.join(form_errors)
+        return super().get_context_data(**kwargs, form_errors=form_errors_str)
+
 
 class LogOutView(BaseLogOutView):
     next_page = 'inventory:index'
