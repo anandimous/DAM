b7d5181efcd8823dee5707aa73226fb27d604384
Aniruddha Nandi <anandi@buffalo.edu>
2018-11-03 21:31:50 -0400

    Deploy (#122)
    * fixed dependency

--- dam/settings.py
+++ dam/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
+import dj-database-url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,5 +137,6 @@ LOGIN_URL = 'users:log-in'
 LOGIN_REDIRECT_URL = 'showDash'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj-database-url.config(conn_max_age=500)
+
 DATABASES['default'].update(db_from_env)
