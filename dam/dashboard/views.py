83cc324d902ff19c829e0fae51c3af3a24825609
Lawreen <lawreenl@buffalo.edu>
2018-11-14 18:55:24 -0500

    html/css dashboard including client loans and reservations

--- dam/dashboard/views.py
+++ dam/dashboard/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from dam.loans.models import ItemReservation, ItemLoan, Client
+
 
 @login_required
 def showDash(request):
-    return render(request, 'dashboard/dashboard.html')
-
+    res = ItemReservation.objects.filter(is_active=True)
+    loans = ItemLoan.objects.filter(returned_at__isnull=True)
+    args = {'reserves': res,
+            'returns': loans}
+    return render(request, 'dashboard/dashboard.html', args)
