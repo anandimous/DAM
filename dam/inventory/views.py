f0a34273d6fd06cb088e5a00b23cd9f374f04b7b
Lawzeem <lawzeeml@buffalo.edu>
2018-11-13 20:06:09 -0500

    Changes made to Search and added search bar to inventory page

--- dam/inventory/views.py
+++ dam/inventory/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from ..inventory import models
 from django.db.models import Q
 
-def index(request):
-    items = models.Item.objects.with_availability()
-    args={'items': items}
-    return render(request, 'inventory/inventory.html', args)
 
 def search(request):
     template = 'inventory/inventory.html'
-    query = request.GET.get('q')
-    results = models.Item.objects.with_availability().filter(Q(name__icontains = query) | Q(description__icontains = query))
-    args = {
-        'items':results
-    }
+    items = models.Item.objects.with_availability()
+    args = {'items': items}
 
+    query = request.GET.get('q')
+    if query == None:
+        return render(request, template, args)
+    else:
+        results = items.filter(Q(name__icontains = query) | Q(description__icontains = query))
+        args = {
+            'items':results
+        }
     return render(request, template, args)
 
 def item_details(request, item_id):
--- dam/search/views.py
+++ dam/search/views.py
@@ -1,15 +1,4 @@
 from django.shortcuts import render
-from django.db.models import Q
-from ..inventory import models
+
 def index(request):
     return render(request, 'search/index.html')
-
-
-def search(request):
-    template = 'inventory/inventory.html'
-    query = request.GET.get('q')
-    results = models.ItemManager.filter(Q(name__icontains = query) | Q(description__icontains = query))
-    context ={
-        'items':results
-    }
-    return render(request, template, context)
