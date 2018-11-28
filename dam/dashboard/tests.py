aeae31e4bd22cf7e6fc1af697606c41bfa2aef43
Stephen James <stephenorjames@gmail.com>
2018-11-28 16:45:29 -0500

    Remove dashboard test that doesn't test dashboard
    This test does not specifically test the dashboard, but rather, the
    behavior of the log in view. Since there is already a test for this
    in the users app, I'm removing this rather than moving it.

--- dam/dashboard/tests.py
+++ dam/dashboard/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from dam.users.models import User
 
-class dashboardTest(TestCase):
+
+class DashboardTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         User.objects.create_user(
@@ -13,16 +14,3 @@ class dashboardTest(TestCase):
     def test_page_login_required(self):
         response = self.client.get('/dashboard/')
         self.assertEquals(response.status_code, 302)
-
-    def test_correct_login_redirect(self):
-        self.client.post(
-            '/users/log-in/',
-            {
-                'email': 'test@example.com',
-                'password': 'password',
-            },
-            follow=True,
-        )
-        response = self.client.get('/dashboard/')
-        self.assertEquals(response.status_code, 200)
-        self.assertContains(response, 'Dashboard')
