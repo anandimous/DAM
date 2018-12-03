7addf30ffb477cc00001d1322cc0d4f0f90df6d5
Lawreen <lawreenl@buffalo.edu>
2018-12-02 21:01:09 -0500

    filtering moved to views.py

--- dam/dashboard/views.py
+++ dam/dashboard/views.py
@@ -5,8 +5,11 @@ from dam.loans.models import ItemReservation, ItemLoan, Client
 
 @login_required
 def showDash(request):
+    user = request.user.email
     res = ItemReservation.objects.filter(is_active=True)
+    cres= res.filter(client__email=user)
     loans = ItemLoan.objects.filter(returned_at__isnull=True)
-    args = {'reserves': res,
-            'returns': loans}
+    cloan = loans.filter(client__email=user)
+    args = {'reserves': cres,
+            'returns': cloan}
     return render(request, 'dashboard/dashboard.html', args)
