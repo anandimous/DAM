4b30e0eab84f933db0b779ead32090c818773db1
Lawreen <lawreenl@buffalo.edu>
2018-12-05 17:25:16 -0500

    changed queries for dashboard

--- dam/dashboard/views.py
+++ dam/dashboard/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from dam.loans.models import ItemReservation, ItemLoan, Client
+from django.db.models import Q
 
 
 @login_required
 def showDash(request):
     user = request.user.email
-    res = ItemReservation.objects.filter(is_active=True)
-    cres= res.filter(client__email=user)
+    cres = ItemReservation.objects.filter(is_active=True)
+    query = request.GET.get('q')
+    if query is not None:
+        cres = cres.filter(
+            Q(client__email=query)
+            |Q(client__first__name=query)
+        )
     loans = ItemLoan.objects.filter(returned_at__isnull=True)
     cloan = loans.filter(client__email=user)
     args = {'reserves': cres,
