beaa8dfa179857bc95f55234671500dec3105d81
Stephen James <stephenorjames@gmail.com>
2018-12-05 03:31:42 -0500

    Stop using form when reserving items
    Users must be authenticated, so there is no longer a need to take the name or email address.

--- dam/loans/tests.py
+++ dam/loans/tests.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from unittest import mock
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+import pytz
 
 from dam.inventory.models import Item
 from dam.loans.models import Client, ItemLoan, ItemReservation
@@ -74,7 +76,7 @@ class ReserveItemTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(
+        cls.user = User.objects.create_user(
             email='example@buffalo.edu',
             password='password',
         )
@@ -82,11 +84,6 @@ class ReserveItemTests(TestCase):
     def test_anonymous_user(self):
         response = self.client.post(
             '/loans/reserve/1/',
-            {
-                'email': 'example@buffalo.edu',
-                'first_name': 'First',
-                'last_name': 'Last',
-            },
             follow=True,
         )
 
@@ -94,61 +91,50 @@ class ReserveItemTests(TestCase):
 
     def test_invalid_item(self):
         self.client.login(username='example@buffalo.edu', password='password')
-
         response = self.client.post(
             '/loans/reserve/1/',
-            {
-                'email': 'example@buffalo.edu',
-                'first_name': 'First',
-                'last_name': 'Last',
-            },
             follow=True,
         )
 
         self.assertEqual(response.status_code, 404)
 
     def test_valid_and_available(self):
-        self.client.login(username='example@buffalo.edu', password='password')
-
         # Create item to be reserved.
         Item.objects.create(id=2, quantity=2)
 
+        reserved_at = datetime(2018, 12, 7, tzinfo=pytz.utc)
+
         # Reserve the item.
-        response = self.client.post(
-            '/loans/reserve/2/',
-            {
-                'email': 'example@buffalo.edu',
-                'first_name': 'First',
-                'last_name': 'Last',
-            },
-            follow=True,
-        )
+        self.client.login(username='example@buffalo.edu', password='password')
+        with mock.patch('django.utils.timezone.now', mock.Mock(return_value=reserved_at)):
+            response = self.client.post(
+                '/loans/reserve/2/',
+                follow=True,
+            )
 
         # Verify that the correct data was stored.
         self.assertEqual(ItemReservation.objects.count(), 1)
         reservation = ItemReservation.objects.first()
+        self.assertEqual(Client.objects.count(), 1)
+        client = Client.objects.first()
         self.assertEqual(reservation.item_id, 2)
-        self.assertEqual(reservation.client, Client(id=1))
+        self.assertEqual(reservation.client, client)
+        self.assertEqual(client.user, self.user)
         self.assertTrue(reservation.is_active)
+        self.assertEqual(reservation.reserved_at, reserved_at)
 
         # Verify that the user got the expected behavior.
         self.assertRedirects(response, '/inventory/details/2/')
         self.assertContains(response, 'The item has been reserved!')
 
     def test_valid_and_unavailable(self):
-        self.client.login(username='example@buffalo.edu', password='password')
-
         # Create item to be reserved.
         Item.objects.create(id=2, quantity=0)
 
         # Reserve the item.
+        self.client.login(username='example@buffalo.edu', password='password')
         response = self.client.post(
             '/loans/reserve/2/',
-            {
-                'email': 'example@buffalo.edu',
-                'first_name': 'First',
-                'last_name': 'Last',
-            },
             follow=True,
         )
 
@@ -158,26 +144,6 @@ class ReserveItemTests(TestCase):
         # Verify that the user got the expected behavior.
         self.assertRedirects(response, '/inventory/details/2/')
         self.assertContains(response, 'The item you tried to reserve is not available.')
-
-    def test_invalid_email(self):
-        self.client.login(username='example@buffalo.edu', password='password')
-
-        # Create item to be reserved.
-        Item.objects.create(id=2, quantity=2)
-
-        # Reserve the item.
-        response = self.client.post(
-            '/loans/reserve/2/',
-            {
-                'email': 'example@invalid.edu',
-                'first_name': 'First',
-                'last_name': 'Last',
-            },
-            follow=True,
-        )
-
-        # Verify that the reservation was not created.
-        self.assertEqual(ItemReservation.objects.count(), 0)
 
 
 class ClientTests(TestCase):
