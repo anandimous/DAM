1deaddb497c72536744bac3844884c86d8b4bfea
Lawreen <lawreenl@buffalo.edu>
2018-12-05 22:01:49 -0500

    updated querying to account for new client info

--- dam/dashboard/views.py
+++ dam/dashboard/views.py
@@ -3,24 +3,20 @@ from django.contrib.auth.decorators import login_required
 from dam.loans.models import ItemReservation, ItemLoan, Client
 from django.db.models import Q
 
-
 @login_required
 def showDash(request):
-    user = request.user.email
-    res = ItemReservation.objects.filter(is_active=True)
-    query = request.GET.get(request)
-    if query is not None:
-        res = res.filter(
-            Q(client__user__email__exact=query)
-            |Q(client__user__first__name__exact=query)
-        )
-    loan = ItemLoan.objects.filter(returned_at__isnull=True)
-    query = request.GET.get(request)
-    if query is not None:
-        loan = loan.filter(
-            Q(client__user__email__exact=query)
-            | Q(client__user__first__name__exact=query)
-        )
-    args = {'reserves': res,
-            'returns': loan}
-    return render(request, 'dashboard/dashboard.html', args)
+    user = request.user
+    reservations = (
+        ItemReservation.objects
+        .filter(is_active=True)
+        .filter(Q(client__user=user) | Q(client__email=user.email))
+    )
+    loans = (
+        ItemLoan.objects
+        .filter(returned_at__isnull=True)
+        .filter(Q(client__user=user) | Q(client__email=user.email))
+    )
+    return render(request, 'dashboard/dashboard.html', {
+        'reserves': reservations,
+        'returns': loans,
+    })
