3348159f92cf8c797b6dd7a81f4a5b9b2ae73c83
Harshita Girase <harshitagirase97@gmail.com>
2018-12-06 03:29:20 -0500

    Images added

--- dam/urls.py
+++ dam/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,4 @@ urlpatterns = [
     path('loans/', include('dam.loans.urls')),
     path('', include('dam.search.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
