5bad8b8043290b9df1d2a22eb71d3f5e6d969192
Aniruddha Nandi <anandi@buffalo.edu>
2018-12-06 02:35:31 -0500

    implemented category filtering, combined search & filter for items

--- dam/inventory/models.py
+++ dam/inventory/models.py
@@ -25,3 +25,11 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    items = models.ManyToManyField(Item, blank=True)
+
+    def __str__(self):
+        return self.name
