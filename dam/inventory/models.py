0d9a0fe0d09e1251792169c49f7255103cf6a677
Aamel Unia <aameluni@buffalo.edu>
2019-10-03 19:45:11 -0400

    Updating item model with blankable fields and displaying item_id on UI to user

--- dam/inventory/models.py
+++ dam/inventory/models.py
@@ -29,29 +29,29 @@ class ItemManager(models.Manager):
         return super().get_queryset().annotate(available=available)
 
 
-class Item(models.Model):
-    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
-    loan_duration = models.DurationField(default=timezone.timedelta(days=14))
-    image = models.ImageField(upload_to="items", default="items/placeholder.png")
-    item_id = models.CharField(max_length=50)
-    serial_number = models.CharField(max_length=100, null=True)
-    metadata = models.TextField(null=True)
-
-    objects = ItemManager()
-
-    def __str__(self):
-        return self.name + ' : ' + self.item_id
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    items = models.ManyToManyField(Item, blank=True)
 
     def __str__(self):
         return self.name
         
     class Meta:
         verbose_name_plural = 'categories'
+
+
+class Item(models.Model):
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    loan_duration = models.DurationField(default=timezone.timedelta(days=14))
+    image = models.ImageField(upload_to="items", default="items/placeholder.png")
+    item_id = models.CharField(max_length=50, blank=True)
+    serial_number = models.CharField(max_length=100, blank=True)
+    metadata = models.TextField(blank=True)
+
+    objects = ItemManager()
+
+    def __str__(self):
+        return self.name + ' : ' + self.item_id
