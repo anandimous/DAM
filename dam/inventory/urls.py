6e2134bc2c90bdb11e254c1d51d2d5dbe466dd40
Aamel Unia <aameluni@buffalo.edu>
2019-11-01 04:19:08 -0400

    Added Download CSV button for downloading inventory

--- dam/inventory/urls.py
+++ dam/inventory/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import item_details, search, get_all_items
+from .views import item_details, search, get_all_items, get_inventory_status_csv
 
 app_name = 'inventory'
 
 urlpatterns = [
     path('', search, name='search'),
     path('details/<int:item_id>/', item_details, name='item-details'),
-    path('list', get_all_items, name='inventory-list')
+    path('list', get_all_items, name='inventory-list'),
+    path('csv', get_inventory_status_csv, name='inventory-csv')
 ]
--- dam/inventory/views.py
+++ dam/inventory/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
@@ -26,7 +28,44 @@ def item_details(request, item_id):
     item = get_object_or_404(Item.objects.with_availability(), pk=item_id)
     return render(request, 'inventory/details.html', {'item': item})
 
+
 @permission_required('loans.change_itemloan')
 def get_all_items(request):
     items = Item.objects.with_availability()
     return render(request, 'inventory/inventory_table.html', {'items': items})
+
+
+def get_inventory_status_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="inventory_status.csv"'
+    # Get all items in inventory
+    items = Item.objects.with_availability()
+
+    writer = csv.writer(response)
+    writer.writerow([
+        'Inventory',
+        'Item',
+        'ID',
+        'Category',
+        'Status',
+        'Pending Reservation',
+        'Loaned On',
+        'Loaned To',
+        'Due On'
+    ])
+
+    for item in items:
+        writer.writerow([
+            item.inventory.name,
+            item.name,
+            item.item_id,
+            item.category,
+            'Available' if item.available > 0 else 'Unavailable',
+            'Yes' if item.active_reservation else 'No',
+            item.active_loan.approved_at if item.active_loan else 'N/A',
+            item.active_loan.user.get_full_name if item.active_loan else 'N/A',
+            item.active_loan.due_on if item.active_loan else 'N/A'
+        ])
+
+    return response
